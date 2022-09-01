3ced996256a5f9d58043bcc85ad832c54d7ce771
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-09-01 13:00:03 +0300

    commit tree 5fc9ad13583cab0cf8665c03f5a2ada4921e3206

--- db/migrate/20220901094647_add_favorite_food_to_artists.rb
+++
@@ -1,5 +0,0 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration[6.1]
-  def change
-    add_column :artists, :favorite_food, :string
-  end
-end
--- db/migrate/20220901095148_addflower.rb
+++
@@ -1,6 +0,0 @@
-class Addflower < ActiveRecord::Migration[6.1]
-  def change
-    add_column :artists, :favorite_flower, :string
-
-  end
-end
